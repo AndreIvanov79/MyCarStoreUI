5d02445d06f3f08cf78131abe819072a75c9a4f4
AndreIvanov79 <dyaka1979@gmail.com>
2022-10-27 19:24:24 +0300

    one abstract table for all comp

--- src/component/rents/rents.js
+++ src/component/rents/rents.js
@@ -1,99 +1,11 @@
-import React, {useEffect, useState} from "react";
+import React from "react";
 import '../styles/category-page.css';
-import {Col, Form, Input, InputNumber, Popconfirm, Row, Table, Typography} from "antd";
-import axios from "axios";
-
-const EditableCell = ({
-                          editing,
-                          dataIndex,
-                          title,
-                          inputType,
-                          record,
-                          index,
-                          children,
-                          ...restProps
-                      }) => {
-    const inputNode = inputType === 'number' ? <InputNumber /> : <Input />;
-    return (
-        <td {...restProps}>
-            {editing ? (
-                <Form.Item
-                    name={dataIndex}
-                    style={{
-                        margin: 0,
-                    }}
-                    rules={[
-                        {
-                            required: true,
-                            message: `Please Input ${title}!`,
-                        },
-                    ]}
-                >
-                    {inputNode}
-                </Form.Item>
-            ) : (
-                children
-            )}
-        </td>
-    );
-};
+import {Col, Row} from "antd";
+import MyTable from "../my-table/my-table";
 
 const Rents = () => {
-    const [form] = Form.useForm();
-    const [data, setData] = useState();
-    const [editingKey, setEditingKey] = useState('');
-
-    useEffect(() => {
-        const apiUrl = '/rents';
-        axios.get(apiUrl,{headers: {'X-Requested-With': 'XMLHttpRequest'}}).then((resp) => {
-            const allRents = resp.data;
-            setData(allRents);
-        });
-    }, [setData]);
-
-    const isEditing = (record) => record.key === editingKey;
-
-    const edit = (record) => {
-        form.setFieldsValue({
-            name: '',
-            age: '',
-            address: '',
-            ...record,
-        });
-        setEditingKey(record.key);
-    };
-
-    const cancel = () => {
-        setEditingKey('');
-    };
-
-    const save = async (key) => {
-        try {
-            const row = await form.validateFields();
-            const newData = [...data];
-            const index = newData.findIndex((item) => key === item.key);
-
-            if (index > -1) {
-                const item = newData[index];
-                newData.splice(index, 1, { ...item, ...row });
-                setData(newData);
-                setEditingKey('');
-            } else {
-                newData.push(row);
-                setData(newData);
-                setEditingKey('');
-            }
-        } catch (errInfo) {
-            console.log('Validate Failed:', errInfo);
-        }
-    };
-
-    const handleDelete = (key) => {
-        const newData = data.filter((item) => item.key !== key);
-        setData(newData);
-    };
-
-    const columns = [
+    const apiUrl = '/rents';
+    const myColumns = [
         {
             title: 'USER',
             dataIndex: 'user',
@@ -124,60 +36,7 @@ const Rents = () => {
             width: '10%',
             editable: true,
         },
-        {
-            title: 'operation',
-            dataIndex: 'operation',
-            render: (_, record) => {
-                const editable = isEditing(record);
-                return editable ? (
-                    <span>
-            <Typography.Link
-                onClick={() => save(record.key)}
-                style={{
-                    marginRight: 8,
-                }}
-            >
-              Save
-            </Typography.Link>
-            <Popconfirm title="Sure to cancel?" onConfirm={cancel}>
-              <a>Cancel</a>
-            </Popconfirm>
-          </span>
-                ) : (
-                    <Typography.Link disabled={editingKey !== ''} onClick={() => edit(record)}>
-                        Edit
-                    </Typography.Link>
-                );
-            },
-        },
-        {
-            title: 'operation',
-            dataIndex: 'operation',
-            render: (_, record) =>
-                data.length >= 1 ? (
-                    <Popconfirm title="Sure to delete?" onConfirm={() => handleDelete(record.key)}>
-                        <a>Delete</a>
-                    </Popconfirm>
-                ) : null,
-        },
     ];
-    const mergedColumns = columns.map((col) => {
-        if (!col.editable) {
-            return col;
-        }
-
-        return {
-            ...col,
-            onCell: (record) => ({
-                record,
-                inputType: col.dataIndex === 'age' ? 'number' : 'text',
-                dataIndex: col.dataIndex,
-                title: col.title,
-                editing: isEditing(record),
-            }),
-        };
-    });
-
 
     return (
         <>
@@ -186,23 +45,7 @@ const Rents = () => {
             </header>
             <Row>
                 <Col span={16} offset={4}>
-
-                    <Form form={form} component={false} className="Table-wrapper">
-                        <Table
-                            components={{
-                                body: {
-                                    cell: EditableCell,
-                                },
-                            }}
-                            bordered
-                            dataSource={data}
-                            columns={mergedColumns}
-                            rowClassName="editable-row"
-                            pagination={{
-                                onChange: cancel,
-                            }}
-                        />
-                    </Form>
+                    <MyTable url={apiUrl} myColumns={myColumns}/>
                 </Col>
             </Row>
         </>
--- src/component/users/users.js
+++ src/component/users/users.js
@@ -1,101 +1,12 @@
-import {Form, Input, InputNumber, Popconfirm, Table, Typography, Col, Row} from 'antd';
-import React, {useEffect, useState} from 'react';
-import '../styles/category-page.css';
-import axios from "axios";
 
-const EditableCell = ({
-                          editing,
-                          dataIndex,
-                          title,
-                          inputType,
-                          record,
-                          index,
-                          children,
-                          ...restProps
-                      }) => {
-    const inputNode = inputType === 'number' ? <InputNumber /> : <Input />;
-    return (
-        <td {...restProps}>
-            {editing ? (
-                <Form.Item
-                    name={dataIndex}
-                    style={{
-                        margin: 0,
-                    }}
-                    rules={[
-                        {
-                            required: true,
-                            message: `Please Input ${title}!`,
-                        },
-                    ]}
-                >
-                    {inputNode}
-                </Form.Item>
-            ) : (
-                children
-            )}
-        </td>
-    );
-};
+import {Col, Row} from 'antd';
+import React from 'react';
+import '../styles/category-page.css';
+import MyTable from "../my-table/my-table";
 
 const Users = () => {
-
-    const [form] = Form.useForm();
-    const [data, setData] = useState();
-    const [editingKey, setEditingKey] = useState('');
-
-    useEffect(() => {
-            const apiUrl = '/users';
-            axios.get(apiUrl,{headers: {'X-Requested-With': 'XMLHttpRequest'}}).then((resp) => {
-                const allUsers = resp.data;
-                setData(allUsers);
-            });
-        }, [setData]);
-
-
-    const isEditing = (record) => record.key === editingKey;
-
-    const edit = (record) => {
-        form.setFieldsValue({
-            name: '',
-            email: '',
-            rents: '',
-            ...record,
-        });
-        setEditingKey(record.key);
-    };
-
-    const cancel = () => {
-        setEditingKey('');
-    };
-
-    const save = async (key) => {
-        try {
-            const row = await form.validateFields();
-            const newData = [...data];
-            const index = newData.findIndex((item) => key === item.key);
-
-            if (index > -1) {
-                const item = newData[index];
-                newData.splice(index, 1, { ...item, ...row });
-                setData(newData);
-                setEditingKey('');
-            } else {
-                newData.push(row);
-                setData(newData);
-                setEditingKey('');
-            }
-        } catch (errInfo) {
-            console.log('Validate Failed:', errInfo);
-        }
-    };
-
-    const handleDelete = (key) => {
-        const newData = data.filter((item) => item.key !== key);
-        setData(newData);
-    };
-
-    const columns = [
+    const apiUrl = '/users';
+    const myColumns = [
         {
             title: 'NAME',
             dataIndex: 'firstName',
@@ -120,66 +31,13 @@ const Users = () => {
             width: '25%',
             editable: true,
         },
-       {
+        {
             title: 'RENT No.',
             dataIndex: 'rents',
             width: '15%',
             editable: true,
-        },
-        {
-            title: 'operation',
-            dataIndex: 'operation',
-            render: (_, record) => {
-                const editable = isEditing(record);
-                return editable ? (
-                    <span>
-            <Typography.Link
-                onClick={() => save(record.key)}
-                style={{
-                    marginRight: 8,
-                }}
-            >
-              Save
-            </Typography.Link>
-            <Popconfirm title="Sure to cancel?" onConfirm={cancel}>
-              <a>Cancel</a>
-            </Popconfirm>
-          </span>
-                ) : (
-                    <Typography.Link disabled={editingKey !== ''} onClick={() => edit(record)}>
-                        Edit
-                    </Typography.Link>
-                );
-            },
-        },
-        {
-            title: 'operation',
-            dataIndex: 'operation',
-            render: (_, record) =>
-                data.length >= 1 ? (
-                    <Popconfirm title="Sure to delete?" onConfirm={() => handleDelete(record.key)}>
-                        <a>Delete</a>
-                    </Popconfirm>
-                ) : null,
-        },
-    ];
-    const mergedColumns = columns.map((col) => {
-        if (!col.editable) {
-            return col;
         }
-        console.log("COL: "+col.dataIndex);
-        return {
-            ...col,
-            onCell: (record) => ({
-                record,
-                inputType: col.dataIndex === 'age' ? 'number' : 'text',
-                dataIndex: col.dataIndex,
-                title: col.title,
-                editing: isEditing(record),
-            }),
-        };
-    });
-
+    ];
 
     return (
         <>
@@ -188,23 +46,7 @@ const Users = () => {
             </header>
             <Row>
                 <Col span={16} offset={4}>
-
-                    <Form form={form} component={false} className="Table-wrapper">
-                        <Table
-                            components={{
-                                body: {
-                                    cell: EditableCell,
-                                },
-                            }}
-                            bordered
-                            dataSource={data}
-                            columns={mergedColumns}
-                            rowClassName="editable-row"
-                            pagination={{
-                                onChange: cancel,
-                            }}
-                        />
-                    </Form>
+                    <MyTable url={apiUrl} myColumns={myColumns}/>
                 </Col>
             </Row>
         </>
