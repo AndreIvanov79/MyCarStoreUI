de1a506020e0e056bce26ec5e71930d57a053c91
AndreIvanov79 <dyaka1979@gmail.com>
2022-10-27 19:24:24 +0300

    one abstract table for all comp

--- src/component/cars/cars.js
+++ src/component/cars/cars.js
@@ -1,110 +1,22 @@
-import React, {useEffect, useState} from "react";
-import {Col, Form, Input, InputNumber, Popconfirm, Row, Table, Typography} from "antd";
-import axios from "axios";
+import React from "react";
+import {Col,  Row} from "antd";
+import MyTable from "../my-table/my-table";
 
-const EditableCell = ({
-                          editing,
-                          dataIndex,
-                          title,
-                          inputType,
-                          record,
-                          index,
-                          children,
-                          ...restProps
-                      }) => {
-    const inputNode = inputType === 'number' ? <InputNumber /> : <Input />;
-    return (
-        <td {...restProps}>
-            {editing ? (
-                <Form.Item
-                    name={dataIndex}
-                    style={{
-                        margin: 0,
-                    }}
-                    rules={[
-                        {
-                            required: true,
-                            message: `Please Input ${title}!`,
-                        },
-                    ]}
-                >
-                    {inputNode}
-                </Form.Item>
-            ) : (
-                children
-            )}
-        </td>
-    );
-};
 
 const Cars = () => {
-    const [form] = Form.useForm();
-    const [data, setData] = useState();
-    const [editingKey, setEditingKey] = useState('');
-
-    useEffect(() => {
-        const apiUrl = '/cars';
-        axios.get(apiUrl,{headers: {'X-Requested-With': 'XMLHttpRequest'}}).then((resp) => {
-            const allCars = resp.data;
-            setData(allCars);
-        });
-    }, [setData]);
-
-    const isEditing = (record) => record.key === editingKey;
-
-    const edit = (record) => {
-        form.setFieldsValue({
-            model: '',
-            number: '',
-            type: '',
-            rented: '',
-            ...record,
-        });
-        setEditingKey(record.key);
-    };
-
-    const cancel = () => {
-        setEditingKey('');
-    };
-
-    const save = async (key) => {
-        try {
-            const row = await form.validateFields();
-            const newData = [...data];
-            const index = newData.findIndex((item) => key === item.key);
-
-            if (index > -1) {
-                const item = newData[index];
-                newData.splice(index, 1, { ...item, ...row });
-                setData(newData);
-                setEditingKey('');
-            } else {
-                newData.push(row);
-                setData(newData);
-                setEditingKey('');
-            }
-        } catch (errInfo) {
-            console.log('Validate Failed:', errInfo);
-        }
-    };
-
-    const handleDelete = (key) => {
-        const newData = data.filter((item) => item.key !== key);
-        setData(newData);
-    };
-
-    const columns = [
+    const apiUrl = '/cars';
+    const myColumns = [
         {
-            title: 'MODEL',
+            title: 'BRAND',
             dataIndex: 'brand',
             width: '25%',
             editable: true,
         },
         {
-          title: 'NUMBER',
-          dataIndex: 'number',
-          width: '25%',
-          editable: true,
+            title: 'NUMBER',
+            dataIndex: 'number',
+            width: '25%',
+            editable: true,
         },
         {
             title: 'TYPE',
@@ -117,61 +29,8 @@ const Cars = () => {
             dataIndex: 'rented',
             width: '15%',
             editable: true,
-        },
-        {
-            title: 'operation',
-            dataIndex: 'operation',
-            render: (_, record) => {
-                const editable = isEditing(record);
-                return editable ? (
-                    <span>
-            <Typography.Link
-                onClick={() => save(record.key)}
-                style={{
-                    marginRight: 8,
-                }}
-            >
-              Save
-            </Typography.Link>
-            <Popconfirm title="Sure to cancel?" onConfirm={cancel}>
-              <a>Cancel</a>
-            </Popconfirm>
-          </span>
-                ) : (
-                    <Typography.Link disabled={editingKey !== ''} onClick={() => edit(record)}>
-                        Edit
-                    </Typography.Link>
-                );
-            },
-        },
-        {
-            title: 'operation',
-            dataIndex: 'operation',
-            render: (_, record) =>
-                data.length >= 1 ? (
-                    <Popconfirm title="Sure to delete?" onConfirm={() => handleDelete(record.key)}>
-                        <a>Delete</a>
-                    </Popconfirm>
-                ) : null,
-        },
-    ];
-    const mergedColumns = columns.map((col) => {
-        if (!col.editable) {
-            return col;
         }
-
-        return {
-            ...col,
-            onCell: (record) => ({
-                record,
-                inputType: col.dataIndex === 'number' ? 'number' : 'text',
-                dataIndex: col.dataIndex,
-                title: col.title,
-                editing: isEditing(record),
-            }),
-        };
-    });
-
+    ];
 
     return (
         <>
@@ -180,23 +39,7 @@ const Cars = () => {
             </header>
             <Row>
                 <Col span={16} offset={4}>
-
-                    <Form form={form} component={false} className="Table-wrapper">
-                        <Table
-                            components={{
-                                body: {
-                                    cell: EditableCell,
-                                },
-                            }}
-                            bordered
-                            dataSource={data}
-                            columns={mergedColumns}
-                            rowClassName="editable-row"
-                            pagination={{
-                                onChange: cancel,
-                            }}
-                        />
-                    </Form>
+                    <MyTable url={apiUrl} myColumns={myColumns}/>
                 </Col>
             </Row>
         </>
